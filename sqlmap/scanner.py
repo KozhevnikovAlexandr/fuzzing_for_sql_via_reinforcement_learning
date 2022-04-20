5dd0e7afaffc78e360d9f499e80e011483355b9d
OlegGubanov <o-gybanov@mail.ru>
2022-04-20 11:16:56 +0500

    Update scanner.py

--- sqlmap/scanner.py
+++ sqlmap/scanner.py
@@ -2,9 +2,7 @@ import os
 
 print("URL:")
 url = input()  # Target URL.
-batch = True  # Never ask for user input, use the default behaviour. Valid: True or False
-forms = True  # Parse and test forms on target URL. Valid: True or False
-crawlDepth = 2  # Crawl the website starting from the target URL. Valid: integer
+
 answers = (
     "sitemap=y",  # do you want to check for the existence of site's sitemap(.xml) [y/N]
     "follow=y",  # got a 302 redirect to '...'. Do you want to follow? [Y/n]
@@ -14,22 +12,32 @@ answers = (
     "other DBMSes=y",  # it looks like the back-end DBMS is '...'. Do you want to skip test payloads specific for other DBMSes? [Y/n]
     "remaining tests=y",  # for the remaining tests, do you want to include all tests for 'MySQL' extending provided level (1) and risk (1) values? [Y/n]
     "keep testing=y",  # POST parameter '...' is vulnerable. Do you want to keep testing the others (if any)? [y/N]
-    "exploit=n",  # do you want to exploit this SQL injection? [Y/n]
+    "exploit=y",  # do you want to exploit this SQL injection? [Y/n]
     "skip further tests=n",  # SQL injection vulnerability has already been detected against '...'. Do you want to skip further tests involving it? [Y/n]
-    "threads=1"  # please enter number of threads? [Enter for 1 (current)]
+    "threads=1",  # please enter number of threads? [Enter for 1 (current)]
+    "multiple injection points=0"  # there were multiple injection points, please select the one to use for following injections
 )
+
 options = {
-    "--batch": batch,
-    "--forms": forms,
-    "--crawl=": crawlDepth > 0,
-    "--answers=": len(answers) > 0
+    "--batch": True,  # Never ask for user input, use the default behaviour. Valid: True or False
+    "--forms": True,  # Parse and test forms on target URL. Valid: True or False
+    "--dbs": True,  # Enumerate DBMS databases
+    "--tables": True,  # Enumerate DBMS database tables
+    "--columns": True,  # Enumerate DBMS database table columns
+    "--crawl=": 2,  # Crawl the website starting from the target URL. Valid: integer
+    "--answers=": answers  # Set predefined answers (e.g. "quit=N,follow=N")
 }
+
 command = "python sqlmap.py -u \"{}\"".format(url)
 for option in options:
-    if options[option]:
-        command = command + " {}".format(option)
-        if option == "--crawl=":
-            command += str(crawlDepth)
-        if option == "--answers=":
-            command += "\"{}\"".format(','.join(answers))
+    value = options[option]
+    if value:
+        command += " "
+        if type(value) is bool:
+            command += option
+        if type(value) is int:
+            command += option + str(value)
+        if type(value) is tuple:
+            command += option + "\"{}\"".format(','.join(answers))
+print(command)
 os.system(command)
